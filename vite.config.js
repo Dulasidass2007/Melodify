cc4d9e4a46dd49d3099fc5f74054eb90b5504c12
Dulasidass V M <vmdulasi@gmail.com>
2025-11-26 14:48:31 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Use repo base when building for production (GitHub Pages),
+  // but use root when running `npm run dev` locally.
+  base: process.env.NODE_ENV === 'production' ? '/Melodify/' : '/'
 })
